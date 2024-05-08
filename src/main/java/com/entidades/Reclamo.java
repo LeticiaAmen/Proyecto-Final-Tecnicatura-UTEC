13374560f32900d20de1a7dc5a5870296a91ac20
leticia.amen <letiamen23@gmail.com>
2024-05-08 14:34:11 -0300

    Cambios en entidad Reclamo

--- src/main/java/com/entidades/Reclamo.java
+++ src/main/java/com/entidades/Reclamo.java
@@ -17,9 +17,12 @@ public class Reclamo implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@SequenceGenerator(name = "seq_reclamo", sequenceName = "seq_reclamo", allocationSize = 1, initialValue = 1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator = "seq_reclamo")
 	@Column(name="ID_RECLAMO")
 	private long idReclamo;
 
+	@Column(name="DETALLE")
 	private String detalle;
 
 	@Temporal(TemporalType.DATE)
