1feb79397ac7cce92111fe4b7cd5e0bea32fc16b
leticia.amen <letiamen23@gmail.com>
2024-05-09 17:13:07 -0300

    Editar Itrs

--- src/main/java/com/servlets/SvEditarITR.java
+++ src/main/java/com/servlets/SvEditarITR.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,59 +12,87 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.entidades.Estado;
 import com.entidades.Itr;
+import com.servicios.EstadoService;
 import com.servicios.ItrService;
 
 @WebServlet("/SvEditarITR")
 public class SvEditarITR extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @EJB
-    private ItrService itrService;
+	@EJB
+	private ItrService itrService;
 
-    public SvEditarITR() {
-        super();
-    }
+	@EJB
+	private EstadoService estadoService;
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        long idItr = Long.parseLong(request.getParameter("IdItr"));
+	public SvEditarITR() {
+		super();
+	}
 
-        Itr itr = itrService.obtenerItr(idItr);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		long idItr = Long.parseLong(request.getParameter("IdItr"));
 
-        request.setAttribute("itr", itr);
+		Itr itr = itrService.obtenerItr(idItr);
+		if (itr == null) {
+			System.out.println("Error al obtener el itr para editar: ITR no encontrado");
+			response.sendRedirect("errorPage.jsp"); // Redirige a una página de error si el ITR es null
+			return;
+		}
+		
+		List<Estado> estados = estadoService.obtenerEstados();
+		request.setAttribute("estados", estados);
+		request.setAttribute("itrParaEditar", itr);
 
-        request.getRequestDispatcher("editarItr.jsp").forward(request, response);
-    }
+		request.getRequestDispatcher("editarItr.jsp").forward(request, response);
+	}
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-        long idItr = Long.parseLong(request.getParameter("IdItr"));
-        String nombre = request.getParameter("nombre");
-        Estado estado = Estado.valueOf(request.getParameter("estado"));
-        
+		String formSubmitted = request.getParameter("formSubmitted");
+		if ("true".equals(formSubmitted)) {
+			long idItr = Long.parseLong(request.getParameter("IdItr"));
+			String nombre = request.getParameter("nombre");
+			String idestadoStr = request.getParameter("idEstado");
+			Long idEstado = Long.parseLong(idestadoStr);  // convertir a long
+			Estado estadoSeleccionado = estadoService.obtenerEstadoId(idEstado);  // obtener la entidad
 
-        Itr existente = itrService.obtenerItrDesdeBaseDeDatosNombre(nombre);
-        if (existente != null && existente.getIdItr() != idItr) {
-            String errorMessage = "Ya existe un ITR con este nombre";
-            request.setAttribute("errorMessage", errorMessage);
-            Itr itr = itrService.obtenerItr(idItr);
-            request.setAttribute("itr", itr);
-            request.getRequestDispatcher("editarItr.jsp").forward(request, response);
-            return; 
-        }
+			// Obtener el ITR actual para comparar el nombre
+	        Itr itrActual = itrService.obtenerItr(idItr);
+	        if (itrActual == null) {
+	            System.out.println("Error al obtener Itr para la modificación");
+	            response.sendRedirect("SvEditarITR");
+	            return;
+	        }
+	        
+	     // Verificar si el nombre ya existe en otro ITR
+			Itr existente = itrService.obtenerItrDesdeBaseDeDatosNombre(nombre);
+			if (existente != null && existente.getIdItr() != idItr) {
+				String errorMessage = "Ya existe un ITR con este nombre.";
+				request.setAttribute("errorMessage", errorMessage);
+				request.getSession().setAttribute("errorMessage", "Ya existe un ITR con este nombre.");
+				response.sendRedirect("ListadoItr");
+			} else {
+				//Actualizar campos del ITR
+				itrActual.setNombre(nombre);
+				itrActual.setEstado(estadoSeleccionado);
+				try {
+					itrService.actualizarItr(itrActual);
+					request.getSession().setAttribute("successMessage", "El ITR se ha modificado exitosamente.");
+					response.sendRedirect("ListadoItr");
+				} catch (Exception e) {
+					e.printStackTrace();
+					System.out.println("Error al modificar Itr");
+					response.sendRedirect("ListadoItr");
+				}
+			}
 
-        Itr itr = itrService.obtenerItr(idItr);
+		}else {
+			// Si el formulario no ha sido enviado, redirige al doGet para cargar la página
+			doGet(request, response);
+		}
 
-        itr.setNombre(nombre);
-        itr.getIdItr();
 
-        itrService.actualizarItr(itr);
-
-        List<Itr> listarITR = itrService.obtenerItrTodos();
-
-        request.setAttribute("itrs", listarITR);
-        
-        request.getRequestDispatcher("listarITR.jsp").forward(request, response);
-    }
+	}
 }
