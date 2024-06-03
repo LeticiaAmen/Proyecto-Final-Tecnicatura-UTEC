4c0423801b9c12b1e4b5dfe5dbaf72db7d4a6a22
mario.couto <mario.couto@estudiantes.utec.edu.uy>
2024-06-03 15:52:52 -0300

    redirección al listado de justificaciones

--- src/main/java/com/servlets/SvJustificarInasistencia.java
+++ src/main/java/com/servlets/SvJustificarInasistencia.java
@@ -54,57 +54,55 @@ public class SvJustificarInasistencia extends HttpServlet {
 		request.getRequestDispatcher("justificarInasistencia.jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String formSubmitted = request.getParameter("formSubmitted");
-		if ("true".equals(formSubmitted)) {
-			//Recuperar el id del estudiante de la sesión
-			String idEstudianteStr = request.getParameter("idEstudiante");
-			long idEstudiante = Long.parseLong(idEstudianteStr);
+	    String formSubmitted = request.getParameter("formSubmitted");
+	    if ("true".equals(formSubmitted)) {
+	        //Recuperar el id del estudiante de la sesión
+	        String idEstudianteStr = request.getParameter("idEstudiante");
+	        long idEstudiante = Long.parseLong(idEstudianteStr);
 
-			String detalle = request.getParameter("detalle");
-			// Obtener la fecha actual en el servidor para el registro del reclamo
-			Date fechaJustificacion = new Date();
+	        String detalle = request.getParameter("detalle");
+	        // Obtener la fecha actual en el servidor para el registro del reclamo
+	        Date fechaJustificacion = new Date();
 
-			// Obtener el ID del evento seleccionado en el formulario
-			String idEventoStr = request.getParameter("idEvento");
-			Long idEvento = Long.parseLong(idEventoStr);
-			Evento evento = eventoService.obtenerEvento(idEvento);
+	        // Obtener el ID del evento seleccionado en el formulario
+	        String idEventoStr = request.getParameter("idEvento");
+	        Long idEvento = Long.parseLong(idEventoStr);
+	        Evento evento = eventoService.obtenerEvento(idEvento);
 
-			// Obtener el estudiante de la base de datos usando el ID almacenado en la sesión
-			Estudiante estudiante = (Estudiante) usuarioService.obtenerUsuario(idEstudiante);
+	        // Obtener el estudiante de la base de datos usando el ID almacenado en la sesión
+	        Estudiante estudiante = (Estudiante) usuarioService.obtenerUsuario(idEstudiante);
 
-			  // Se lo setea en 1 que es el estado 'Ingresado'
+	        // Se lo setea en 1 que es el estado 'Ingresado'
 	        RegistroAccione registroAccion = registroAccionService.obtenerRegistroAccion(1);		
 			
-			//obtener estado
-			Estado estadoActivo = estadoService.obtenerEstadoId(1);
+	        //obtener estado
+	        Estado estadoActivo = estadoService.obtenerEstadoId(1);
 
-			//crear la justificacion
-			Justificacion justificacion = new Justificacion();
-			justificacion.setDetalle(detalle);
-			justificacion.setFechaHora(fechaJustificacion);
-			justificacion.setEvento(evento);
-			justificacion.setEstudiante(estudiante);
-			justificacion.setEstado(estadoActivo);
-			justificacion.setRegistroAccione(registroAccion);
+	        //crear la justificacion
+	        Justificacion justificacion = new Justificacion();
+	        justificacion.setDetalle(detalle);
+	        justificacion.setFechaHora(fechaJustificacion);
+	        justificacion.setEvento(evento);
+	        justificacion.setEstudiante(estudiante);
+	        justificacion.setEstado(estadoActivo);
+	        justificacion.setRegistroAccione(registroAccion);
 
-			try {
-				justificacionService.crearJustificacion(justificacion);
-				// Atributo de sesión para mostrar un mensaje de éxito en la interfaz de usuario
-				request.getSession().setAttribute("ingresoExitoso", "Su justificacion ha sido enviada con éxito.");
-				response.sendRedirect("menuEstudiante.jsp");
-
-			}catch(Exception e) {
-				e.printStackTrace();
-				response.sendRedirect("menuEstudiante.jsp");
-			}
-		} else {
-			// Si no se envió el formulario, recargar la página de registro de reclamo
-			doGet(request, response);
-		}
+	        try {
+	            justificacionService.crearJustificacion(justificacion);
+	            // Atributo de sesión para mostrar un mensaje de éxito en la interfaz de usuario
+	            request.getSession().setAttribute("ingresoExitoso", "Su justificación ha sido enviada con éxito.");
+	            response.sendRedirect("SvListarJustificaciones");
+	        } catch(Exception e) {
+	            e.printStackTrace();
+	            response.sendRedirect("menuEstudiante.jsp");
+	        }
+	    } else {
+	        // Si no se envió el formulario, recargar la página de registro de reclamo
+	        doGet(request, response);
+	    }
 	}
+
 
 }
